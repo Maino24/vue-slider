4050554add6c4f94d2cebbfc6206bff4c54108ae
Maino24 <mg02.gabriele@gmail.com>
2022-11-11 16:06:51 +0100

    creato oggetto di array

--- main.js
+++ main.js
@@ -2,7 +2,17 @@ var app = new Vue(
     {
         el: `#app`,
         data: {
-            linkImage: `https://picsum.photos/200`
+            
+            obj: [
+  
+                {linkImage:  `https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`},
+                {linkImage: `https://i.picsum.photos/id/938/200/300.jpg?hmac=MVXKrDXBUPK5fv_Ev3FTdCFeYf9rvJE2Tz9xynjeelM`},
+                {linkImage: `https://i.picsum.photos/id/659/200/300.jpg?hmac=cmMJe403Rt0WMoriAFlgDaHI4FkwevCOXFyhnelzolY`},
+                {linkImage: `https://i.picsum.photos/id/389/200/300.jpg?hmac=XjGcjYBMJ1aWzqOK5SWp_oZ2nU6KgS1PPzuQXJvRcJ4`},
+                {linkImage: `https://i.picsum.photos/id/61/200/300.jpg?hmac=4gnmCaXyXsOzE8pxb43yUtdfZnVbnUSGdPaJdh-aCUo`}
+            
+            ]
+            
         }
     }
 
