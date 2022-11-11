511d9cb0dc5f8b32ca3e7360b859bcb8eba31d6d
Maino24 <mg02.gabriele@gmail.com>
2022-11-11 16:32:32 +0100

    stampato ciclo di immagini

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ var app = new Vue(
         el: `#app`,
         data: {
             
-            obj: [
+            images: [`https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`, `https://i.picsum.photos/id/938/200/300.jpg?hmac=MVXKrDXBUPK5fv_Ev3FTdCFeYf9rvJE2Tz9xynjeelM`, `https://i.picsum.photos/id/659/200/300.jpg?hmac=cmMJe403Rt0WMoriAFlgDaHI4FkwevCOXFyhnelzolY`,
+         `https://i.picsum.photos/id/389/200/300.jpg?hmac=XjGcjYBMJ1aWzqOK5SWp_oZ2nU6KgS1PPzuQXJvRcJ4`, `https://i.picsum.photos/id/61/200/300.jpg?hmac=4gnmCaXyXsOzE8pxb43yUtdfZnVbnUSGdPaJdh-aCUo` ]
+            
+         
+         /*obj: [
   
                 {linkImage:  `https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`},
                 {linkImage: `https://i.picsum.photos/id/938/200/300.jpg?hmac=MVXKrDXBUPK5fv_Ev3FTdCFeYf9rvJE2Tz9xynjeelM`},
@@ -11,9 +15,16 @@ var app = new Vue(
                 {linkImage: `https://i.picsum.photos/id/389/200/300.jpg?hmac=XjGcjYBMJ1aWzqOK5SWp_oZ2nU6KgS1PPzuQXJvRcJ4`},
                 {linkImage: `https://i.picsum.photos/id/61/200/300.jpg?hmac=4gnmCaXyXsOzE8pxb43yUtdfZnVbnUSGdPaJdh-aCUo`}
             
-            ]
+            ]*/
             
+        },
+
+        methods:{
+
+
         }
+
+
     }
 
 )
